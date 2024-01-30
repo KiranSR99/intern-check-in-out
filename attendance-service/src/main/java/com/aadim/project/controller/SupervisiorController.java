51062827d7e08b796f3e560c3e1791b4c17da0ea
Dhiraj <jireldhiraj123@gmail.com>
2024-01-30 17:11:40 +0545

    supervisior curd

--- attendance-service/src/main/java/com/aadim/project/controller/SupervisiorController.java
+++ attendance-service/src/main/java/com/aadim/project/controller/SupervisiorController.java
@@ -1,10 +1,13 @@
 package com.aadim.project.controller;
 
+import com.aadim.project.controller.Base.BaseController;
+import com.aadim.project.dto.GlobalApiResponse;
 import com.aadim.project.dto.request.SupervisiorRequest;
 import com.aadim.project.dto.response.SupervisiorResponse;
 import com.aadim.project.service.impl.SupervisiorServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,21 +15,21 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/supervisior")
-public class SupervisiorController {
+public class SupervisiorController extends BaseController {
 
     @Autowired
     private SupervisiorServiceImpl supervisiorService;
 
     @PostMapping("/add")
-    public SupervisiorResponse addSupervisior(SupervisiorRequest supervisiorRequest) {
+    public ResponseEntity<GlobalApiResponse> addSupervisior(SupervisiorRequest supervisiorRequest) {
 
-        return supervisiorService.addSupervisior(supervisiorRequest);
+        return successResponse(supervisiorService.addSupervisior(supervisiorRequest));
     }
 
 
     @GetMapping("/get")
-    public List<SupervisiorResponse> getSupervisior() {
-        return supervisiorService.getSupervisior();
+    public ResponseEntity<GlobalApiResponse> getSupervisior() {
+        return successResponse(supervisiorService.getSupervisior());
     }
 
     @GetMapping("/get/{id}")

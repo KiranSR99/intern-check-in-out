35e49b4b0ba4d715a34ea3912d2d7029d8c9c00f
ChitraPrsd7 <chitraprasadacharya550@gmail.com>
2024-01-30 16:47:04 +0545

    try pull the code

--- attendance-ui/src/app/app-routing.module.ts
+++ attendance-ui/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 
 const routes: Routes = [
@@ -10,7 +11,7 @@ const routes: Routes = [
   
   { path: 'login', component: LoginComponent },
 
-  
+  { path: 'dashboard', component:DashboardComponent },
 ];
 
 

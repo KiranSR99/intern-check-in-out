e9914cbf2d75143cbc2dd03e5c51e8c03c2698d9
Kiran Shrestha <kiranxtha99@gmail.com>
2024-01-30 11:10:26 +0545

    folder structure made

--- attendance-ui/src/app/app.module.ts
+++ attendance-ui/src/app/app.module.ts
@@ -1,17 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BaseComponent } from './components/base/base.component';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BaseComponent,
+    SideNavComponent,
+    DashboardComponent,
+    LoginComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
   ],
+  
   providers: [],
   bootstrap: [AppComponent]
 })

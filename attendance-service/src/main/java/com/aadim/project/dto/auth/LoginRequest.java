074a7709f2f33b9a26c0f1974fc782194067a739
= <=>
2024-01-31 12:33:11 +0545

    Admin user created in init db

--- attendance-service/src/main/java/com/aadim/project/dto/auth/LoginRequest.java
+++ attendance-service/src/main/java/com/aadim/project/dto/auth/LoginRequest.java
@@ -1,5 +1,12 @@
 package com.aadim.project.dto.auth;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class LoginRequest {
     private String email;
     private String password;

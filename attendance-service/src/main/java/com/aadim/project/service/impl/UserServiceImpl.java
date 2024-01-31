cdc7c8a7d63d9339e52287acafadda0b446998fb
dahalutsab <dlutsab2120@gmail.com>
2024-01-31 10:22:59 +0545

    user

--- attendance-service/src/main/java/com/aadim/project/service/impl/UserServiceImpl.java
+++ attendance-service/src/main/java/com/aadim/project/service/impl/UserServiceImpl.java
@@ -8,6 +8,7 @@ import com.aadim.project.entity.Login;
 import com.aadim.project.entity.Supervisor;
 import com.aadim.project.repository.AdminRepository;
 import com.aadim.project.repository.InternRepository;
+import com.aadim.project.repository.LoginRepository;
 import com.aadim.project.repository.SupervisorRepository;
 import com.aadim.project.service.UserService;
 import jakarta.transaction.Transactional;
@@ -24,24 +25,32 @@ public class UserServiceImpl implements UserService {
     private final AdminRepository adminRepository;
     private final InternRepository internRepository;
     private final SupervisorRepository supervisorRepository;
+    private final LoginRepository loginRepository;
+
     @Override
     @Transactional
     public UserResponse saveUser(UserRequest request) {
         Login login = new Login();
         login.setEmail(request.getEmail());
         login.setPassword(new BCryptPasswordEncoder().encode(request.getPassword()));
+        login.setRole(request.getRole());
+        loginRepository.save(login);
+
         if(Objects.equals(request.getRole(), "ADMIN")) {
             Admin admin = new Admin();
             admin.setFullName(request.getFullName());
             admin.setPhone(request.getPhone());
+            adminRepository.save(admin);
         } else if (Objects.equals(request.getRole(), "SUPERVISIOR")) {
             Supervisor supervisor = new Supervisor();
             supervisor.setFullName(request.getFullName());
             supervisor.setPhone(request.getPhone());
+            supervisorRepository.save(supervisor);
         } else if (Objects.equals(request.getRole(), "INTERN")) {
             Intern intern = new Intern();
             intern.setFullName(request.getFullName());
             intern.setPhone(request.getPhone());
+            internRepository.save(intern);
         }
         return new UserResponse();
     }

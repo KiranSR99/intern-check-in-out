7d88e02f00860d3dcd5860f9115be1d7fb1d7658
Pasang60 <pasanggelbu@gmail.com>
2024-01-31 18:17:09 +0545

    Get all details created

--- attendance-service/src/main/java/com/aadim/project/service/ScheduleService.java
+++ attendance-service/src/main/java/com/aadim/project/service/ScheduleService.java
@@ -2,13 +2,18 @@ package com.aadim.project.service;
 
 import com.aadim.project.dto.request.ScheduleRequest;
 import com.aadim.project.dto.request.ScheduleUpdateRequest;
+import com.aadim.project.dto.response.ScheduleDetailResponse;
 import com.aadim.project.dto.response.ScheduleResponse;
+
+import java.util.List;
 
 public interface ScheduleService {
 
     ScheduleResponse saveCheckIn(ScheduleRequest request);
 
     ScheduleResponse updateCheckOut(ScheduleUpdateRequest request);
+
+    List<ScheduleDetailResponse> fetchAll();
 
 //    @Transactional
 //    ScheduleResponse updateUserCheckoutTime(Integer userId);

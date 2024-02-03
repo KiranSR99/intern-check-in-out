e089d43e22882d685db7d01f536ce1dcbf87aa95
Kiran Shrestha <kiranxtha99@gmail.com>
2024-02-03 12:08:11 +0545

    changes

--- attendance-ui/src/app/components/header/header.component.ts
+++ attendance-ui/src/app/components/header/header.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  userRole: any;
 
   constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.userRole = localStorage.getItem('role');
+
+    if (this.userRole) {
+      this.userRole = JSON.parse(this.userRole);
+    }
+  }
 
   logout() {
     // Remove token from local storage
@@ -16,4 +25,12 @@ export class HeaderComponent {
     this.router.navigate(['/']);
   }
 
+  onLeaveRequestClick() {
+    if(this.userRole == 'INTERN'){
+      this.router.navigate(['/app/log-mgnt/leave-request'])
+    }
+    else{
+      this.router.navigate(['/app/log-mgnt/leave-request-list'])
+    }
+  }
 }

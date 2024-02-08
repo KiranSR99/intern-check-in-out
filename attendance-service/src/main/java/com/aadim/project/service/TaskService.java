c43e4eafade9097b23a195978522d527da48ba4a
= <=>
2024-02-08 16:28:20 +0545

    Search by name and search by time implemented

--- attendance-service/src/main/java/com/aadim/project/service/TaskService.java
+++ attendance-service/src/main/java/com/aadim/project/service/TaskService.java
@@ -3,18 +3,24 @@ package com.aadim.project.service;
 import com.aadim.project.dto.request.TaskRequest;
 import com.aadim.project.dto.request.TaskUpdateRequest;
 import com.aadim.project.dto.response.TaskResponse;
+import com.aadim.project.entity.Task;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface TaskService {
 
     List<TaskResponse> saveAllTasks(TaskRequest  taskRequest);
 
-    List<TaskResponse> getAllTasks();
+    Page<TaskResponse> getAllTasks(Pageable pageable);
 
     TaskResponse getTaskById(Integer id);
 
     List<TaskResponse> getTaskOfOneUser(Integer userId);
 
     TaskResponse updateTask(TaskUpdateRequest updateRequest);
+
+    List<Task> searchTasks(LocalDateTime localDateTime);
 }

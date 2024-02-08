e77a8537c0b09ed295826cea033748120fd6c04e
Dhiraj <jireldhiraj123@gmail.com>
2024-02-08 07:52:25 +0545

    create pagable response, set authority for leave

--- attendance-service/src/main/java/com/aadim/project/service/LeaveService.java
+++ attendance-service/src/main/java/com/aadim/project/service/LeaveService.java
@@ -2,13 +2,15 @@ package com.aadim.project.service;
 
 import com.aadim.project.dto.request.LeaveRequest;
 import com.aadim.project.dto.response.LeaveResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface LeaveService {
-    String createLeave(LeaveRequest leave);
+    String createLeave(LeaveRequest leave) throws RuntimeException;
 
-    List<LeaveResponse> getAllLeaves();
+    Page<LeaveResponse> getAllLeaves(Pageable pageable);
 
     LeaveResponse getLeaveById(Integer id);
 

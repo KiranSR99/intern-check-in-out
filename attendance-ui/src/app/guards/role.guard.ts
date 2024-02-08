613bd83818aa5bb3e76d257dbd1fd48ef6381d18
Kiran Shrestha <kiranxtha99@gmail.com>
2024-02-08 15:12:58 +0545

    pagination

--- attendance-ui/src/app/guards/role.guard.ts
+++ attendance-ui/src/app/guards/role.guard.ts
@@ -1,14 +1,32 @@
-import { inject } from '@angular/core';
-import { CanActivateFn, Router } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { CanActivate, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
-export const roleGuard: CanActivateFn = (route, state) => {
-  const role = localStorage.getItem('role');
-  const router = inject(Router);
+@Injectable({
+  providedIn: 'root'
+})
+export class RoleGuard implements CanActivate {
 
-  if (role !== 'ADMIN') {
-    router.navigate(['/access-denied']);
-    return false;
+  constructor(private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
+    const role = localStorage.getItem('role');
+    const path = route.routeConfig?.path;
+
+    if (role === 'ADMIN' && this.isAdminRoute(path)) {
+      return true;
+    } else if (role === 'INTERN' && this.isInternRoute(path)) {
+      return true;
+    } else {
+      this.router.navigate(['/access-denied']);
+      return false;
+    }
   }
 
-  return true;
-};
+  private isAdminRoute(path: string | undefined): any {
+    return path === 'user-list' || path === 'add-user' || path?.startsWith('update-user');
+  }
+
+  private isInternRoute(path: string | undefined): boolean {
+    return path === 'add-log' || path === 'edit-log' || path === 'leave-request' || path === 'create-new' || path === 'my-log';
+  }
+}
---
+++ attendance-ui/src/app/routes.ts
@@ -0,0 +1,4 @@
+interface RouteItem {
+    path: string;
+    roles: string[];
+}
